177ad9a8d45c077d363984c76b71cc1e6fd797bb
Scott Zeta <scott-zeta@outlook.com>
2025-03-21 22:02:54 +1030

    Add Career Anchor Data interface and questions

---
+++ src/lib/CareerAnchorData.ts
@@ -0,0 +1,153 @@
+interface Question {
+  id: number;
+  text: string;
+  anchor: string;
+  anchorLabel: string;
+}
+
+export const questions: Question[] = [
+  {
+    id: 1,
+    text: 'I want to be really good at my job, one of the best, an expert.',
+    anchor: 'TF',
+    anchorLabel: 'Technical/Functional (TF)',
+  },
+  {
+    id: 2,
+    text: 'I really feel most satisfied when I am able to manage the work of others to achieve a common goal.',
+    anchor: 'GMC',
+    anchorLabel: 'General Managerial Competence (GMC)',
+  },
+  {
+    id: 3,
+    text: 'Ideally I want to do things my way and to my own timetable.',
+    anchor: 'AI',
+    anchorLabel: 'Autonomy/Independence (AI)',
+  },
+  {
+    id: 4,
+    text: "I would much rather build my own business than be the boss in someone else's.",
+    anchor: 'EC',
+    anchorLabel: 'Entrepreneurial Creativity (EC)',
+  },
+  {
+    id: 5,
+    text: 'I believe that security and stability are much more important than having the freedom to choose how I work.',
+    anchor: 'SS',
+    anchorLabel: 'Security/Stability (SS)',
+  },
+  {
+    id: 6,
+    text: 'My ideal career will enable me to integrate all of my needs – whether work, personal or family.',
+    anchor: 'LS',
+    anchorLabel: 'Lifestyle (LS)',
+  },
+  {
+    id: 7,
+    text: 'It is important to me that I use my talents to further the greater good.',
+    anchor: 'S',
+    anchorLabel: 'Service/Dedication to a Cause (S)',
+  },
+  {
+    id: 8,
+    text: 'I get a kick out of solving the unsolvable or winning against the odds.',
+    anchor: 'PC',
+    anchorLabel: 'Pure Challenge (PC)',
+  },
+  {
+    id: 9,
+    text: 'I will only feel really successful when I have the freedom to define my work.',
+    anchor: 'AI',
+    anchorLabel: 'Autonomy/Independence (AI)',
+  },
+  {
+    id: 10,
+    text: 'I feel most satisfied and fulfilled when I am able to use my expertise, talents and skills.',
+    anchor: 'TF',
+    anchorLabel: 'Technical/Functional (TF)',
+  },
+  {
+    id: 11,
+    text: 'I would really like to start my own business one day.',
+    anchor: 'EC',
+    anchorLabel: 'Entrepreneurial Creativity (EC)',
+  },
+  {
+    id: 12,
+    text: 'I would be very uncomfortable working in an organisation that took a lot of risks. I prefer to work for an organisation that offers stability and security.',
+    anchor: 'SS',
+    anchorLabel: 'Security/Stability (SS)',
+  },
+  {
+    id: 13,
+    text: 'I would rather seek employment elsewhere than move to a role that seriously undermined my ability to serve the greater good/others.',
+    anchor: 'S',
+    anchorLabel: 'Service/Dedication to a Cause (S)',
+  },
+  {
+    id: 14,
+    text: 'I prefer to work on projects that really challenge my problem-solving skills and have a competitive element.',
+    anchor: 'PC',
+    anchorLabel: 'Pure Challenge (PC)',
+  },
+  {
+    id: 15,
+    text: 'I would rather find a new job than accept a role that puts constraints on how I do my work.',
+    anchor: 'AI',
+    anchorLabel: 'Autonomy/Independence (AI)',
+  },
+  {
+    id: 16,
+    text: 'Balancing my work with my family and personal commitments is more important to me than a senior position.',
+    anchor: 'LS',
+    anchorLabel: 'Lifestyle (LS)',
+  },
+  {
+    id: 17,
+    text: 'One day I would like to be the boss, in charge of a whole organisation.',
+    anchor: 'GMC',
+    anchorLabel: 'General Managerial Competence (GMC)',
+  },
+  {
+    id: 18,
+    text: 'Reaching a position of seniority in my area of expertise is far more important to me than becoming a more senior general manager.',
+    anchor: 'TF',
+    anchorLabel: 'Technical/Functional (TF)',
+  },
+  {
+    id: 19,
+    text: 'I want to make a difference in my career. I will only be truly satisfied if I feel I have made a real contribution to society.',
+    anchor: 'S',
+    anchorLabel: 'Service/Dedication to a Cause (S)',
+  },
+  {
+    id: 20,
+    text: 'Working on difficult problems is more important to me than achieving a high-level position.',
+    anchor: 'PC',
+    anchorLabel: 'Pure Challenge (PC)',
+  },
+  {
+    id: 21,
+    text: 'My preference in choosing a role would be to seek out opportunities that minimise any interference with my personal life (family, friends etc).',
+    anchor: 'LS',
+    anchorLabel: 'Lifestyle (LS)',
+  },
+  {
+    id: 22,
+    text: 'I would feel really fulfilled if I was able to create an enterprise that was primarily the result of my ingenuity, skills and efforts.',
+    anchor: 'EC',
+    anchorLabel: 'Entrepreneurial Creativity (EC)',
+  },
+  {
+    id: 23,
+    text: 'Job security and financial independence are really important to me.',
+    anchor: 'SS',
+    anchorLabel: 'Security/Stability (SS)',
+  },
+  {
+    id: 24,
+    text: 'I would rather become a general manager with broader responsibilities than become a senior functional manager in my area of expertise.',
+    anchor: 'GMC',
+    anchorLabel: 'General Managerial Competence (GMC)',
+  },
+];
